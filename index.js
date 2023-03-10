ecc8a1a529d80b74c47e10cfefe8cfd8c331801d
Kane-Nguyen <khayeuem1000@gmail.com>
2023-03-10 22:45:49 +0700

    upload

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb",extended: true}));
 app.use(cors(corsOptions));
 app.use('/posts' , postRoutes);
 app.use('/user' , userRoutes);
+app.get('/', (req, res) =>{
+    res.send("APP IS RUNNING");
+});
 
 
 
