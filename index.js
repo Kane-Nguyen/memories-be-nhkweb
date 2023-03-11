70315e8c9849989d3546a83d0d359ff02f40b8eb
Kane-Nguyen <106100341+Kane-Nguyen@users.noreply.github.com>
2023-03-11 21:53:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 dotenv.config();
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb+srv://kaneblog123:Kha123abcd@cluster0.70y58df.mongodb.net/memories_blog", {
+mongoose.connect("mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.70y58df.mongodb.net/memories_blog", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
