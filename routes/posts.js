8d09e62f105a1e9f364bd4819bf3dfbb4e20b5a3
Kane-Nguyen <106100341+Kane-Nguyen@users.noreply.github.com>
2023-03-12 18:55:30 +0700

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -6,12 +6,9 @@ import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-
+router.get('/',getPosts);
 router.get('/search',getPostsBySearch);
 router.get('/:id',getPost);
-
-router.get('/',getPosts);
-
 router.post('/',auth, createPost);
 router.patch(`/:id`,auth,updatePost);
 router.delete(`/:id`,auth, deletePost);
