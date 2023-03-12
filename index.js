caadfad21343d8795774ade8f39cb89ca79d84e1
Kane-Nguyen <106100341+Kane-Nguyen@users.noreply.github.com>
2023-03-12 18:36:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 const corsOptions ={
-    origin:'http://memoriesblog.click', 
+    origin:'https://memoriesblog.click', 
     credentials:true,   
     optionSuccessStatus:200
 }
